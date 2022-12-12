d97d8cf7cfc8b9c2e62b30cd20bcbb8f81edf06e
user <you@example.com>
2022-12-12 23:49:41 +0300

    Fixed task6 ATM

--- src/task_3/ATM.java
+++ src/task_3/ATM.java
@@ -15,7 +15,7 @@ public class ATM {
 
 
     public ATM() {
-        this(1000, 1000, 1000, 500);
+        this(1000, 1000, 1000, 10000);
     }
 
     public ATM(int amountBill_20, int amountBill_50, int amountBill_100, int oneOperationSum) {
@@ -26,7 +26,7 @@ public class ATM {
         this.sumAllMoneyInATM += amountBill_20 * BILL_20 + amountBill_50 * BILL_50 + amountBill_100 * BILL_100;
     }
 
-    public void setMoney(int bill_20, int bill_50, int bill_100) {
+    public void addMoney(int bill_20, int bill_50, int bill_100) {
         amountBill_20 += bill_20;
         amountBill_50 += bill_50;
         amountBill_100 += bill_100;
@@ -34,37 +34,64 @@ public class ATM {
     }
 
     public boolean getMoney(int cashSum) {
-        boolean bol;
-        int countBill;
+        int countBill100 = cashSum / BILL_100;
+        int countBill50 = 1;
+        int countBill20;
         System.out.println("There are banknotes with a face values of 20, 50, 100 at the ATM," +
                 " enter the sum no more then " + oneOperationSum + ".");
         System.out.println("Required sum: " + cashSum);
         if ((cashSum % BILL_100 == 0) && (cashSum <= oneOperationSum)) {
-            countBill = cashSum / BILL_100;
-            amountBill_100 -= countBill;
-            sumAllMoneyInATM -= countBill * BILL_100;
-            setMoney(0,0,countBill);
-            System.out.println("You have got " + countBill + " banknotes with a face value of " + BILL_100);
-            bol = true;
+            amountBill_100 -= countBill100;
+            sumAllMoneyInATM -= countBill100 * BILL_100;
+            System.out.println("You have got " + countBill100 + " banknotes with a face value of " + BILL_100);
+            System.out.println(this);
+            addMoney(0, 0, countBill100);
+            return true;
         } else if ((cashSum % BILL_50 == 0) && (cashSum <= oneOperationSum)) {
-            countBill = cashSum / BILL_50;
-            amountBill_50 -= countBill;
-            sumAllMoneyInATM -= countBill * BILL_50;
-            setMoney(0,countBill,0);
-            System.out.println("You have got " + countBill + " banknotes with a face value of " + BILL_50);
-            bol = true;
+            amountBill_100 -= countBill100;
+            amountBill_50 -= countBill50;
+            sumAllMoneyInATM -= (countBill50 * BILL_50 + countBill100 * BILL_100);
+            System.out.println("You have got " + countBill100 + " banknotes with a face value of " + BILL_100 + " " +
+                    "and " + countBill50 + " banknotes with a face value of " + BILL_50);
+            System.out.println(this);
+            addMoney(0, countBill50, countBill100);
+            return true;
         } else if ((cashSum % BILL_20 == 0) && (cashSum <= oneOperationSum)) {
-            countBill = cashSum / BILL_20;
-            amountBill_20 -= countBill;
-            sumAllMoneyInATM -= countBill * BILL_20;
-            setMoney(countBill,0,0);
-            System.out.println("You have got " + countBill + " banknotes with a face value of " + BILL_20);
-            bol = true;
+            countBill20 = (cashSum - countBill100 * BILL_100) / BILL_20;
+            amountBill_100 -= countBill100;
+            amountBill_20 -= countBill20;
+            sumAllMoneyInATM -= (countBill20 * BILL_20 + countBill100 * BILL_100);
+            System.out.println("You have got " + countBill100 + " banknotes with a face value of " + BILL_100 + " " +
+                    "and " + countBill20 + " banknotes with a face value of " + BILL_20);
+            System.out.println(this);
+            addMoney(countBill20, 0, countBill100);
+            return true;
+        } else if ((cashSum == 110) || (cashSum == 130)) {
+            countBill20 = (cashSum - BILL_50) / BILL_20;
+            amountBill_50 -= countBill50;
+            amountBill_20 -= countBill20;
+            sumAllMoneyInATM -= (countBill20 * BILL_20 + countBill50 * BILL_50);
+            System.out.println("You have got " + countBill50 + " banknotes with a face value of " + BILL_50 + " " +
+                    "and " + countBill20 + " banknotes with a face value of " + BILL_20);
+            System.out.println(this);
+            addMoney(countBill20, countBill50, 0);
+            return true;
+        } else if ((cashSum % 10 == 0) && (cashSum > 30) && (cashSum <= oneOperationSum)) {
+            countBill20 = (cashSum - BILL_50 - countBill100 * BILL_100) / BILL_20;
+            amountBill_100 -= countBill100;
+            amountBill_50 -= countBill50;
+            amountBill_20 -= countBill20;
+            sumAllMoneyInATM -= (countBill20 * BILL_20 + countBill50 * BILL_50 + countBill100 * BILL_100);
+            System.out.println("You have got " + countBill100 + " banknotes with a face value of " + BILL_100 + " " +
+                    ", " + countBill50 + " banknotes with a face value of " + BILL_50 + " and " + countBill20 + " " +
+                    "banknotes with a face value of " + BILL_20);
+            System.out.println(this);
+            addMoney(countBill20, countBill50, countBill100);
+            return true;
         } else {
             System.out.println("Insert correct sum.");
-            bol = false;
+            return false;
         }
-        return bol;
     }
 
     @Override
--- src/task_3/Test.java
+++ src/task_3/Test.java
@@ -2,26 +2,13 @@ package task_3;
 
 public class Test {
     public static void main(String[] args) {
-        ATM atm1 = new ATM();
-        System.out.println(atm1);
+
+
+        ATM atm = new ATM();
+        System.out.println(atm);
         System.out.println("----------------------------------------------");
-
-        boolean bol = atm1.getMoney(480);
+        boolean bol = atm.getMoney(130);
         System.out.println("----------------------------------------------");
-
-        System.out.println(atm1);
-        System.out.println("----------------------------------------------");
-
-        // создание банкомата с некорректными параметрами:
-
-        ATM atm2 = new ATM(10, 10, 10, 2000);
-        System.out.println(atm2);
-        System.out.println("----------------------------------------------");
-
-        boolean bol1 = atm2.getMoney(1760);
-        System.out.println("----------------------------------------------");
-
-        System.out.println(atm2);
+        System.out.println(atm);
     }
-
 }
